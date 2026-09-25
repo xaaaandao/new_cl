4dee0b86c47731efbb77d2a0f1c59f81c888741d
Alexandre Kajihara <alexandre.ykz@gmail.com>
2026-09-25 18:15:14 -0300

    Update copy.py

--- saved_models/copy.py
+++ saved_models/copy.py
@@ -3,22 +3,22 @@ import pathlib
 import shutil
 
 for p in pathlib.Path(".").rglob("*loss.csv"):
-    filename = "./saved_models/" + p.parent.parent.name + "_" + p.name
+    filename = "./" + p.parent.parent.name + "_" + p.name
     print(f"src: {p}")
     print(f"dst: {filename}")
-    shutil.copy(p, filename)
+    if not os.path.exists(filename):
+        shutil.copy(p, filename)
 
 for p in pathlib.Path(".").rglob("*results+*.csv"):
-    filename = "./saved_models/" + p.parent.parent.name + "_" + p.name
+    filename = "./" + p.parent.parent.name + "_" + p.name
     print(f"src: {p}")
     print(f"dst: {filename}")
-    shutil.copy(p, filename)
+    if not os.path.exists(filename):
+        shutil.copy(p, filename)
 
-for p in pathlib.Path(".").rglob("*.npz"):
-    dst = "./saved_models/" + p.parent.parent.parent.name + "_npz"
+for p in pathlib.Path("./").rglob("features"):
+    filename = "./" + p.parent.name + "_" + p.name
     print(f"src: {p}")
-    os.makedirs(dst, exist_ok=True)
-    filename = os.path.join(dst, p.name)
     print(f"dst: {filename}")
-    shutil.copy(p, filename)
-
+    if not os.path.exists(filename):
+        shutil.copytree(p, filename)
