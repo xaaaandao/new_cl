44680e6a32e16095d74e1d083eddfccccb8c9bec
Alexandre Kajihara <alexandre.ykz@gmail.com>
2026-09-25 18:16:34 -0300

    Update2 copy.py

--- saved_models/copy.py
+++ saved_models/copy.py
@@ -3,21 +3,21 @@ import pathlib
 import shutil
 
 for p in pathlib.Path(".").rglob("*loss.csv"):
-    filename = "./" + p.parent.parent.name + "_" + p.name
+    filename = "./saved_models/" + p.parent.parent.name + "_" + p.name
     print(f"src: {p}")
     print(f"dst: {filename}")
     if not os.path.exists(filename):
         shutil.copy(p, filename)
 
 for p in pathlib.Path(".").rglob("*results+*.csv"):
-    filename = "./" + p.parent.parent.name + "_" + p.name
+    filename = "./saved_models/" + p.parent.parent.name + "_" + p.name
     print(f"src: {p}")
     print(f"dst: {filename}")
     if not os.path.exists(filename):
         shutil.copy(p, filename)
 
 for p in pathlib.Path("./").rglob("features"):
-    filename = "./" + p.parent.name + "_" + p.name
+    filename = "./saved_models/" + p.parent.name + "_" + p.name
     print(f"src: {p}")
     print(f"dst: {filename}")
     if not os.path.exists(filename):
